bd16c037bd1cc456600c77fe83bc3544f63c7e38
SowatRafi <sowad.rafi@icloud.com>
2023-01-07 16:33:20 +0600

    Done with ENUMERATIONS and switch statement.

---
+++ Dart Part/02. enumerations, classes and objects/01. enumerations.dart
@@ -0,0 +1,7 @@
+enum PersonProperties { Sowad, Hossain, Twenty_Four }
+
+void main(List<String> args) {
+  print(PersonProperties.Sowad.name);
+  print(PersonProperties.Hossain.runtimeType);
+  print(PersonProperties.Twenty_Four.hashCode);
+}
