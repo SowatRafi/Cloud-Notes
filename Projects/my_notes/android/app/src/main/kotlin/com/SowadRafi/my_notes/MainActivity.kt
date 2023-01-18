ac438e7062c089ae5e9e9e003ba1a597f786a84f
SowatRafi <sowad.rafi@icloud.com>
2023-01-18 11:25:00 +0600

    Register - Create the user

---
+++ Projects/my_notes/android/app/src/main/kotlin/com/SowadRafi/my_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.SowadRafi.my_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Projects/my_notes/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Notes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // `late` Although this field do not have any value, this field will assigned with value before it is used.
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  // Initial State - One time
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  // Dispose State
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: HintText('xxxxx@xxxxx.xxx'),
+          ),
+          TextField(
+            controller: _password,
+            decoration: HintText('xxxxxxx'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              final userCredential = await FirebaseAuth.instance
+                  .createUserWithEmailAndPassword(
+                      email: email, password: password);
+              print(userCredential);
+            },
+            child: const Text("Register"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  HintText(String txt) {
+    return InputDecoration(hintText: txt);
+  }
+}
