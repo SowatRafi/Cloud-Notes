c7e5a2b7afe25e2126c6f341dd6b82638f6744e1
SowatRafi <sowad.rafi@icloud.com>
2023-01-19 17:23:21 +0600

    Verify Email View if email is not verified

--- Projects/my_notes/lib/main.dart
+++ Projects/my_notes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_notes/firebase_options.dart';
@@ -40,13 +41,19 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text('Done');
-            // } else {
-            //   return const HomePage();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified!");
+              }
+            } else {}
+            return const Text("Done");
+          // if (user?.emailVerified ?? false) {
+          //   return const Text('Done');
+          // } else {
+          //   return const HomePage();
+          // }
+          // return const LoginView();
           default:
             return const CircularProgressIndicator();
         }
