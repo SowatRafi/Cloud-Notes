93bc93ac2e45682497bb54ecf5119cf16fa2d273
SowatRafi <sowad.rafi@icloud.com>
2023-01-20 16:47:33 +0600

    Handle other Firebase authentication exceptions

--- Projects/my_notes/lib/view/login_view.dart
+++ Projects/my_notes/lib/view/login_view.dart
@@ -66,6 +66,8 @@ class _LoginViewState extends State<LoginView> {
                   await showErrorDialog(context, "User not found!");
                 } else if (e.code == 'wrong-password') {
                   await showErrorDialog(context, "Wrong credentials!");
+                } else {
+                  await showErrorDialog(context, 'Error: ${e.code}');
                 }
               }
             },
