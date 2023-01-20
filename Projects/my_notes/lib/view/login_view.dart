9f4d3a33ecd07c9fa5aa698946e0d64625f03d17
SowatRafi <sowad.rafi@icloud.com>
2023-01-20 16:30:46 +0600

    user-not-found handled.

--- Projects/my_notes/lib/view/login_view.dart
+++ Projects/my_notes/lib/view/login_view.dart
@@ -64,7 +64,7 @@ class _LoginViewState extends State<LoginView> {
                     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found');
+                  await showErrorDialog(context, "User not found!");
                 } else if (e.code == 'wrong-password') {
                   devtools.log('Wrong password');
                 }
@@ -85,4 +85,22 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
   }
+}
+
+Future<void> showErrorDialog(BuildContext context, String text) {
+  return showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: const Text("An error occured"),
+          content: Text(text),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Okay!"))
+          ],
+        );
+      }));
 }
