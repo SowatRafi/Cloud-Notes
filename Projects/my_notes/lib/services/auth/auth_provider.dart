623017a96b8fe524c3d96169a6ee713aa07b5f81
SowatRafi <sowad.rafi@icloud.com>
2023-01-21 08:26:22 +0600

    Migrating to Auth service

---
+++ Projects/my_notes/lib/services/auth/auth_provider.dart
@@ -0,0 +1,6 @@
+import 'package:my_notes/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({required String email, required String password});
+}
