e1dd1ec2a3976287939dcd69c9108ab6e74eb911
SowatRafi <sowad.rafi@icloud.com>
2023-01-21 09:49:44 +0600

    From AuthService, delegate to AuthProvider

--- Projects/my_notes/lib/services/auth/auth_service.dart
+++ Projects/my_notes/lib/services/auth/auth_service.dart
@@ -3,39 +3,45 @@ import 'package:my_notes/services/auth/auth_user.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
-  AuthService(this.provider);
+  const AuthService(this.provider);
 
   @override
   Future<AuthUser> createUser({
     required String email,
     required String password,
-  }) {
-    // TODO: implement createUser
-    throw UnimplementedError();
-  }
+  })
+      // createUser
+      =>
+      provider.createUser(
+        email: email,
+        password: password,
+      );
 
   @override
-  // TODO: implement currentUser
-  AuthUser? get currentUser => throw UnimplementedError();
+  // currentUser
+  AuthUser? get currentUser => provider.currentUser;
 
   @override
   Future<AuthUser> logIn({
     required String email,
     required String password,
-  }) {
-    // TODO: implement logIn
-    throw UnimplementedError();
-  }
+  })
+      // logIn
+      =>
+      provider.logIn(
+        email: email,
+        password: password,
+      );
 
   @override
-  Future<void> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
-  }
+  Future<void> logOut()
+      // logOut
+      =>
+      provider.logOut();
 
   @override
-  Future<void> sendEmailVerification() {
-    // TODO: implement sendEmailVerification
-    throw UnimplementedError();
-  }
+  Future<void> sendEmailVerification()
+      // sendEmailVerification
+      =>
+      provider.sendEmailVerification();
 }
