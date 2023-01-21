92eba5fc72567d229b1e0d62c05f769a4ec15412
SowatRafi <sowad.rafi@icloud.com>
2023-01-21 08:58:39 +0600

    FirebaseAuthProvider class -> set the exceptions for createUser ,and check whether the current user is null or not!

---
+++ Projects/my_notes/lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,66 @@
+import 'package:my_notes/services/auth/auth_user.dart';
+import 'package:my_notes/services/auth/auth_provider.dart';
+import 'package:my_notes/services/auth/auth_exception.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    // createUser
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final user = currentUser;
+      if (user != null) {
+        return user;
+      } else {
+        throw UserNotLoggedInAuthException();
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw WeakPasswordAuthException();
+      } else if (e.code == 'email-already-in-use') {
+        throw EmailAlreadyInUseAuthException();
+      } else if (e.code == 'invalid-email') {
+        throw InvalidEmailAuthException();
+      } else {
+        throw GenericAuthException();
+      }
+    } catch (_) {
+      throw GenericAuthException();
+    }
+  }
+
+  @override
+  // currentUser
+  AuthUser? get currentUser {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return AuthUser.fromFirebase(user);
+    }
+  }
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
