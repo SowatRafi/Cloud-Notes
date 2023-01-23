441388abab88860e02a727aed8b7b197503ff7ed
SowatRafi <sowad.rafi@icloud.com>
2023-01-23 19:47:50 +0600

    Test logging out and in again

--- Projects/my_notes/test/auth_test.dart
+++ Projects/my_notes/test/auth_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:my_notes/code_repeat.dart';
 import 'package:my_notes/services/auth/auth_exception.dart';
 import 'package:my_notes/services/auth/auth_provider.dart';
@@ -7,110 +5,103 @@ import 'package:my_notes/services/auth/auth_user.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group(
-    'Mock Authentication',
-    () {
-      final provider = MockAuthProvider();
+  group('Mock Authentication', () {
+    final provider = MockAuthProvider();
 
-      test(
-        'Should not be initialized to begin with',
-        () {
-          expect(
-            provider.isInitialized,
-            false,
-          );
-        },
+    test('Should not be initialized to begin with', () {
+      expect(
+        provider.isInitialized,
+        false,
       );
+    });
 
-      test(
-        "Can't log out if not initialized",
-        () {
-          expect(
-            provider.logOut(),
-            throwsA(const TypeMatcher<NotInitializedExcepttion>()),
-          );
-        },
+    test("Can't log out if not initialized", () {
+      expect(
+        provider.logOut(),
+        throwsA(const TypeMatcher<NotInitializedExcepttion>()),
       );
+    });
 
-      test(
-        'Should able to initialized',
-        () async {
-          await provider.initialize();
-          expect(
-            provider.isInitialized,
-            true,
-          );
-        },
+    test('Should able to initialized', () async {
+      await provider.initialize();
+      expect(
+        provider.isInitialized,
+        true,
       );
+    });
 
-      test(
-        'Should be null after initialization',
-        () {
-          expect(
-            provider.currentUser,
-            null,
-          );
-        },
+    test('Should be null after initialization', () {
+      expect(
+        provider.currentUser,
+        null,
       );
+    });
 
-      test(
-        'Should be able to initialize in less than 2 seconds',
-        () async {
-          await provider.initialize();
-          expect(
-            provider.isInitialized,
-            true,
-          );
-        },
-        timeout: const Timeout(
-          Duration(seconds: 2),
+    test(
+      'Should be able to initialize in less than 2 seconds',
+      () async {
+        await provider.initialize();
+        expect(
+          provider.isInitialized,
+          true,
+        );
+      },
+      timeout: const Timeout(
+        Duration(seconds: 2),
+      ),
+    );
+
+    test('Create user should delegate to logIn function', () async {
+      final badEmailUser = provider.createUser(
+        email: "first.last@email.com",
+        password: "anypassword",
+      );
+      expect(
+        badEmailUser,
+        throwsA(
+          const TypeMatcher<UserNotFoundAuthException>(),
         ),
       );
 
-      test(
-        'Create user should delegate to logIn function',
-        () async {
-          final badEmailUser = provider.createUser(
-            email: "first.last@email.com",
-            password: "anypassword",
-          );
-          expect(
-            badEmailUser,
-            throwsA(
-              const TypeMatcher<UserNotFoundAuthException>(),
-            ),
-          );
-
-          final badPasswordUser = provider.createUser(
-            email: "someone@email.com",
-            password: "password",
-          );
-          expect(
-            badPasswordUser,
-            throwsA(
-              const TypeMatcher<WrongPasswordAuthException>(),
-            ),
-          );
-
-          final user = await provider.createUser(
-            email: "first",
-            password: "pass",
-          );
-          expect(provider.currentUser, user);
-          expect(user.isEmailVerified, false);
-        },
+      final badPasswordUser = provider.createUser(
+        email: "someone@email.com",
+        password: "password",
+      );
+      expect(
+        badPasswordUser,
+        throwsA(
+          const TypeMatcher<WrongPasswordAuthException>(),
+        ),
       );
 
-      test(
-        "Logged in user should be able to get verified",
-        () {
-          provider.sendEmailVerification();
-          final user = provider.currentUser;
-          expect(user, isNotNull);
-        },
+      final user = await provider.createUser(
+        email: "first",
+        password: "pass",
       );
-    },
-  );
+      expect(provider.currentUser, user);
+      expect(user.isEmailVerified, false);
+    });
+
+    test("Logged in user should be able to get verified", () {
+      provider.sendEmailVerification();
+      final user = provider.currentUser;
+      expect(user, isNotNull);
+      expect(user!.isEmailVerified, true);
+    });
+
+    test("Should be able to log out and log in again", () async {
+      await provider.logOut();
+      await provider.logIn(
+        email: 'email',
+        password: 'password',
+      );
+      final user = provider.currentUser;
+      expect(
+        user,
+        isNotNull,
+      );
+    });
+  });
 }
 
 class NotInitializedExcepttion implements Exception {}
