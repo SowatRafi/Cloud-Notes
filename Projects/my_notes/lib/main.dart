91b1fe07c5dd9bc3c02c320fa835b67307436d1a
SowatRafi <sowad.rafi@icloud.com>
2023-01-28 12:25:42 +0600

    Fixed the import loaction of notes_view in main file

--- Projects/my_notes/lib/main.dart
+++ Projects/my_notes/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_notes/constants/routes.dart';
 import 'package:my_notes/services/auth/auth_service.dart';
 import 'package:my_notes/view/login_view.dart';
-import 'package:my_notes/view/notes_view.dart';
+import 'package:my_notes/view/notes/notes_view.dart';
 import 'package:my_notes/view/register_view.dart';
 import 'package:my_notes/view/verify_email_view.dart';
 
