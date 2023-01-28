b5698f49235d4d5aabad6cdc095f01dbe4e882a0
SowatRafi <sowad.rafi@icloud.com>
2023-01-28 19:30:05 +0600

    create a new note

--- Projects/my_notes/lib/view/notes/new_note_view.dart
+++ Projects/my_notes/lib/view/notes/new_note_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_notes/services/auth/auth_service.dart';
 import 'package:my_notes/services/curd/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
@@ -12,6 +13,18 @@ class _NewNoteViewState extends State<NewNoteView> {
   DatabaseNote? _note;
   late final NoteService _notesService;
   late final TextEditingController _textController;
+
+  // create new note
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
+  }
 
   @override
   Widget build(BuildContext context) {
