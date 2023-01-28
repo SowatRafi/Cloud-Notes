1ce9e2d81335f1ad661b4619333f114d4d437d61
SowatRafi <sowad.rafi@icloud.com>
2023-01-28 19:38:32 +0600

    Take care of the new_note_view's disposal

--- Projects/my_notes/lib/view/notes/new_note_view.dart
+++ Projects/my_notes/lib/view/notes/new_note_view.dart
@@ -46,6 +46,15 @@ class _NewNoteViewState extends State<NewNoteView> {
     }
   }
 
+  // disposal
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
