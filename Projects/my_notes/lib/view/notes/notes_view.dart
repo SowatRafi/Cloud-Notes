f822445212023d4ae71567fad0e3eab5c2b053cb
SowatRafi <sowad.rafi@icloud.com>
2023-01-31 11:28:21 +0600

    Removed showLogOutDialog from NotesView since we are going to have generic one

--- Projects/my_notes/lib/view/notes/notes_view.dart
+++ Projects/my_notes/lib/view/notes/notes_view.dart
@@ -74,20 +74,6 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final allNotes = snapshot.data as List<DatabaseNote>;
-                        return ListView.builder(
-                          itemCount: allNotes.length,
-                          itemBuilder: (context, index) {
-                            final note = allNotes[index];
-                            return ListTile(
-                              title: Text(
-                                note.text,
-                                maxLines: 1,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            );
-                          },
-                        );
                       } else {
                         return const CircularProgressIndicator();
                       }
@@ -103,30 +89,4 @@ class _NotesViewState extends State<NotesView> {
       ),
     );
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text("Sign out"),
-        content: const Text("Are you sure that you want to sign out?"),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-            child: const Text("Cancel"),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text("Logout"),
-          )
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
