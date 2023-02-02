6cbf7dde81cb209a51197af23d11251c2515dea7
SowatRafi <sowad.rafi@icloud.com>
2023-02-02 12:11:02 +0600

    Changed the name of the application to Cloud

--- Projects/my_notes/lib/main.dart
+++ Projects/my_notes/lib/main.dart
@@ -11,7 +11,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-      title: 'My Note',
+      title: 'My Cloud Notes',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
