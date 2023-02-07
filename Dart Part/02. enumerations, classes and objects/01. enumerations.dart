5e496c6c4751320a0d5728d0b0a3043caf1e7ba8
SowatRafi <sowad.rafi@icloud.com>
2023-02-07 19:04:01 +0600

    Some folder deleted and moved the main project here.

--- Dart Part/02. enumerations, classes and objects/01. enumerations.dart
+++
@@ -1,7 +0,0 @@
-enum PersonProperties { Sowad, Hossain, Twenty_Four }
-
-void main(List<String> args) {
-  print(PersonProperties.Sowad.name);
-  print(PersonProperties.Hossain.runtimeType);
-  print(PersonProperties.Twenty_Four.hashCode);
-}
